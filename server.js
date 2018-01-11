3639dee386e06c2593722d723aa4a4c516f92a15
CodeDealerDreamer <olfa-DEV@users.noreply.github.com>
2018-01-11 12:44:57 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+console.log('Hello world!')
+
+var express = require('express')
+var app = express()
+var http = require('http');
+
+ 
+app.get('/', function (req, res) {
+  res.sendFile(__dirname+'/public/'+'home.html')
+})
+
+app.get('/:page', function (req, res) //:page pour passer une variable
+{
+  var p = req.params.page;
+  console.log(req.params);
+  res.sendFile(__dirname+'/public/'+p+'.html')
+})
+
+
+//  app.get('/quotes', function (req, res) {
+//    res.sendFile(__dirname+'/public/'+'quotes.html')
+//  })
+
+// app.get('/aboutus', function (req, res) {
+//   res.sendFile(__dirname+'/public/'+'aboutus.html')
+// })
+
+// app.get('/contact', function (req, res) {
+//   res.sendFile(__dirname+'/public/'+'contact.html')
+// })
+ 
+app.listen(3000)
